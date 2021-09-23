36c530e341252c7615cd72b3f9c5e084148b2bc5
xuhao <bingoxuhao@gmail.com>
2021-09-23 12:17:32 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import App from './App';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+  let n = 124
+  return (
+    <div className="App">
+      爸爸
+      <Son messageForSon={n%2 === 0 ? '偶数' : '奇数'}/>
+    </div>
+  )
+}
+class Son extends React.Component {
+  render(){
+    return (
+      <div className="Son">
+        爸爸对儿子说：{this.props.messageForSon}
+        <Grandson messageForGrandson="孙子你好"/>
+      </div>
+    )
+  }
+}
+const Grandson = (props) => {
+  return (
+    <div className="Grandson">
+      爸爸对儿子说，你爷爷让我跟你说：{props.messageForGrandson}
+    </div>
+  )
+}
+// class Son extends React.Component {
+//   constructor(){
+//     super()
+//     this.state = {
+//       n: 0
+//     }
+//   }
+//   add(){
+//     this.setState({n: this.state.n + 1})
+//   }
+//   render(){
+//     return (
+//       <div className="Son">
+//         爸爸 n：{this.state.n}
+//         <button onClick={()=>this.add()}>+1</button>
+//         <Grandson />
+//       </div>
+//     )
+//   }
+// }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// function Grandson(){
+//   const [n, setN] = React.useState(0)
+//   return (
+//     <div className="Grandson">
+//       孙子 n：{n}
+//       <button onClick={() => setN(n + 1)}>+1</button>
+//     </div>
+//   )
+// }
+
+ReactDOM.render(<App />,document.getElementById('root'));
