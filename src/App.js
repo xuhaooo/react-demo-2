51d22f0651e46420f7a67310f7482c7da814a52d
xuhao <bingoxuhao@gmail.com>
2021-09-27 17:15:05 +0800

    自己实现 useState

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
-// eslint-disable-next-line
-import React, { useState, useEffect } from 'react'
-import useUpdate from './useUpdate'
 
-const App = props => {
-  const [n, setN] = useState(0)
-  const click = () => {
-    setN(n + 1)
-  }
+import React from 'react'
 
-  useUpdate(()=>{ console.log('变了') }, n)
 
-  return (
-    <div>
-      {n}
-      <button onClick={click}>+1</button>
-    </div>
-  )
-}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,81 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+let _state = []
+let index = 0
+const myUseState = (initialValue) => {
+  const currentIndex = index
+  _state[currentIndex] = _state[currentIndex] === undefined ? initialValue : _state[currentIndex]
+  const setState = (newValue) => {
+    _state[currentIndex] = newValue
+    reRender()
+  }
+  index += 1
+  console.log(_state[currentIndex])
+  return [_state[currentIndex], setState]
+}
+
+const reRender = () => {
+  index = 0
+  ReactDOM.render(<App/>, document.getElementById('root'))
+}
+
+const App = () => {
+	const [n, setN] = myUseState(0)
+  const [m, setM] = myUseState(0)
+  const [x, setX] = myUseState(0)
+	return (
+    <div>
+      <p>{n}</p>
+      <button onClick={() => setN(n+1)}>+1</button>
+      <p>{m}</p>
+      <button onClick={() => setM(m+1)}>+1</button>
+      <p>{x}</p>
+      <button onClick={() => setX(x+1)}>+1</button>
+    </div>
+	)
+}
+
+
+
+// let _state = []
+// let index = 0
+// const myUseState = (initialValue) => {
+//   const currentIndex = index
+//   _state[currentIndex] = _state[currentIndex] === undefined ? initialValue : _state[currentIndex]
+//   console.log('currentIndex:')
+//   console.log(currentIndex)
+//   console.log('_state:')
+//   console.log(_state)
+//   function setState(newValue){
+//     _state[currentIndex] = newValue
+//     render()
+//   }
+//   index += 1
+//   return [_state[currentIndex], setState]
+// }
+
+
+
+// const render = () => {
+//   index = 0
+//   // eslint-disable-next-line
+//   ReactDOM.render(<App />, document.getElementById('root'))
+// }
+
+
+// const App = () => {
+//   const [n, setN] = myUseState(0)
+//   const [m, setM] = myUseState(0)
+//   return (
+//     <div>
+//       <p>{n}</p>
+//       <button onClick={() => setN(n+1)}>+1</button>
+//       <p>{m}</p>
+//       <button onClick={() => setM(m+1)}>+1</button>
+//     </div>
+//   )
+// }
+
 
 ReactDOM.render(<App />,document.getElementById('root'));
